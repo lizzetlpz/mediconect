f23c184e587f76e1302f18e0f26a4e9dd6e18244
lizze <medicoomx@gmail.com>
2025-12-02 22:32:56 -0700

    FIX: Corregir frontendUrl en environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://mediconect-production.up.railway.app/api',
   wsUrl: 'wss://mediconect-production.up.railway.app',
-  frontendUrl: 'https://mediconect-vercel.vercel.app'
+  frontendUrl: 'https://mediconect.vercel.app'
 };
